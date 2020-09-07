144665c0986e1bded4f3136cd9294f8fdf65446a
Birkhoff <juniorthx@gmail.com>
2020-09-06 20:45:24 -0700

    new updates

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
    .then(response=>response.json())
    .then(response=>{
        const {baseTemperature, monthlyVariance}=response;
+       const data=monthlyVariance.map(d=>({
+           ...d,
+           temp: baseTemperature - d.variance
+       }))
+       
     //XSCALE
     const xScale = d3.scaleTime()
                      .domain([d3.min(monthlyVariance, d=>d.year), d3.max(monthlyVariance, d=>d.year)])
@@ -13,6 +18,7 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
     g.append("g").attr("id","x-axis")
                  .attr("transform","translate(0, "+ height +")")
                  .call(d3.axisBottom(xScale).tickFormat(d3.format("d")).ticks(20));
+
     //YSCALE
     const yScale=d3.scaleLinear()
                    .domain([0, 11])
@@ -22,8 +28,9 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
         date.setUTCMonth(month);
         return d3.timeFormat("%B")(date);
     }));
+    //color
+    const color=d3.scaleOrdinal(["#C0C0C0","#808080",,"#FFFF00", "#808000","#008000","#800000","#FF0000"]);
 
-    const color=d3.scaleOrdinal(["dodgerblue","orange"]);
     //TITLE
     svg.append("text")
         .attr("id","title")
@@ -57,20 +64,20 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
         //const tooltip=d3.select("body").append("div").attr("class","tooltip").attr("id", "tooltip").style("opacity", 0);
   
     g.selectAll("rect")
-       .data(monthlyVariance)
+       .data(data)
        .enter()
        .append("rect")
        .attr("class","cell")
+       .attr("data-month", d=>d.month)
+       .attr("data-year", d=>d.year)
+       .attr("data-temp", d=>d.baseTemperature)
        .attr("x", d=>xScale(d.year))
        .attr("y", d=>yScale(d.month - 1))
        .attr("width", 10)
        .attr("height", 10)
-       .attr("data-month", d=>d.month)
-       .attr("data-year", d=>d.year)
-       .attr("data-temp", d=>d.temp)
-        // .attr("fill",d=>{
-        //         return color(d.Doping !="")
-        //     })
+       .attr("fill",d=>{
+            return d=>color(d.baseTemperature)
+        })
         //     .on("mouseover", d => { 
         //      tooltip.style("opacity", 0.9);
         //      tooltip.attr("data-year", d.Year);
